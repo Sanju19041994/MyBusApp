b92823a4c018eb85ad58125771f9887e11ea41e1
Sanju19041994 <Sanjuchakole.sanju@gmail.com>
2023-07-14 14:17:57 +0530

    MBA-1 : Create SpringBoot Application MyBusApp, whith JPA, MySql Deiver, Spring Web dependencies

---
+++ src/main/java/com/sanju/MyBusAppApplication.java
@@ -0,0 +1,13 @@
+package com.sanju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBusAppApplication.class, args);
+	}
+
+}
